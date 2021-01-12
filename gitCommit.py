78652a099a27d1b22cbb23ab935d5f2345259324
Michael Watts <michaelcwatts123@gmail.com>
2021-01-12 03:01:14 -0600

    update the repository

---
+++ gitCommit.py
@@ -0,0 +1,32 @@
+import subprocess as cmd
+import random
+import string
+import os
+
+def commit():
+    adder = cmd.run("git add .", check=True, shell=True)
+    messages = ['update the repository', 'added new files', 'commiting files', 'general maintence']
+    message = random.choice(messages)
+    adder = cmd.run("git commit -m \'" + message + "\' ", check=True, shell=True)
+    adder = cmd.run("git push -u origin master -f", check=True, shell=True)
+
+def generateFiles():
+    titleVal = random.randint(1,10)
+    contentVal = random.randint(100,1000)
+    lineVal = random.randint(10,70)
+    title = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(titleVal)) + '.txt'
+    f = open(title, "a")
+    for i in range(lineVal):
+        f.write(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(contentVal)))
+        f.write('\n')
+    f.close()   
+
+def clean():
+    filelist = [ f for f in os.listdir('coomitControl/') if f.endswith(".txt") ]
+    for f in filelist:
+        os.remove(os.path.join('coomitControl/', f))
+
+generateFiles()
+commit()
+clean()
+
